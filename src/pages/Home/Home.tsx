322f933b649c8fdbea54bd460537696629ab1631
Maryam <maryam.bahangeh@gmail.com>
2025-01-24 23:25:34 +0100

    [ADD] Hamburger Menu

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,10 +5,25 @@ import Search from "@/components/Search/Search.tsx";
 
 import styles from "./Home.module.css";
 import RoadMap from "@/components/RoadMap/RoadMap.tsx";
+import { useState } from "react";
+import Menu from "@/components/Menu/Menu.tsx";
 
 function Home() {
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
   return (
     <div className={styles.content}>
+      <button
+        className={styles.menu}
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+      >
+        <img
+          className={styles.menu}
+          src="/images/icones/shared/mobile/icon-hamburger.svg"
+          alt=""
+        />
+      </button>
+      {isMenuOpen && <Menu onApply={() => setIsMenuOpen(false)} />}
+
       <div className={styles["sidebar"]}>
         <Board />
 
