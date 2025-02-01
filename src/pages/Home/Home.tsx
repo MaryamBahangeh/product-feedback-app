b3b095e1dd4a151a02d6c4ecc1dad81e1106198a
BijanProgrammer <bijaneisapour@gmail.com>
2025-02-01 11:17:27 +0330

    [REFACTOR] sidebar into a separated component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,34 +1,13 @@
+import Sidebar from "@/components/Sidebar/Sidebar.tsx";
 import Toolbar from "@/components/Toolbar/Toolbar.tsx";
 import Suggestions from "@/components/Suggestions/Suggestions.tsx";
-import Board from "@/components/Board/Board.tsx";
-import Search from "@/components/Search/Search.tsx";
 
 import styles from "./Home.module.css";
-import RoadMap from "@/components/RoadMap/RoadMap.tsx";
-import { useState } from "react";
-import Menu from "@/components/Menu/Menu.tsx";
 
 function Home() {
-  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
   return (
     <div className={styles.content}>
-      <button
-        className={styles.menu}
-        onClick={() => setIsMenuOpen(!isMenuOpen)}
-      >
-        <img
-          className={styles.menu}
-          src="/images/icones/shared/mobile/icon-hamburger.svg"
-          alt=""
-        />
-      </button>
-      {isMenuOpen && <Menu onApply={() => setIsMenuOpen(false)} />}
-
-      <div className={styles["sidebar"]}>
-        <Board />
-        <Search className={styles["search"]} />
-        <RoadMap className={styles["road-map"]} />
-      </div>
+      <Sidebar className={styles.sidebar} />
 
       <div className={styles.header}>
         <Toolbar />
