b1711e578cd3f98f64574121c7c15ed2a99a405b
Maryam <maryam.bahangeh@gmail.com>
2025-02-10 21:31:50 +0100

    [ADD] translation.json files for farsi and english

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import "./styles/color.css";
 import "./styles/typography.css";
 import "./index.css";
+import "./i18n";
 
 import App from "./App.tsx";
 
