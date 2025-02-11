5a224afeaa4622d756c36977e1b1415a0259b790
Maryam <maryam.bahangeh@gmail.com>
2025-02-11 08:30:14 +0100

    [ADD] select component

---
+++ src/components/Select/Select.tsx
@@ -0,0 +1,47 @@
+import { ReactElement } from "react";
+
+import { useTranslation } from "react-i18next";
+
+import clsx from "clsx";
+
+import styles from "./Select.module.css";
+import { DropdownOption } from "@/models/dropdown-type.ts";
+
+type Props = {
+  options: DropdownOption[];
+  defaultValue: string;
+  onChange: (option: DropdownOption) => void;
+  className?: string;
+};
+
+export default function Select({
+  options,
+  defaultValue,
+  onChange,
+  className,
+}: Props): ReactElement {
+  const { t } = useTranslation();
+
+  return (
+    <select
+      className={clsx(styles.select, className)}
+      onChange={(e) =>
+        onChange({
+          translationKey: e.target.name,
+          value: e.currentTarget.value,
+        })
+      }
+      defaultValue={defaultValue}
+    >
+      {options.map((option) => (
+        <option
+          className={styles.option}
+          key={option.value}
+          value={option.value}
+        >
+          {t(option.translationKey)}
+        </option>
+      ))}
+    </select>
+  );
+}
