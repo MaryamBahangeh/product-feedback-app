1c989593031a225dd6dae89939de7737cd65ecea
Maryam <maryam.bahangeh@gmail.com>
2025-02-21 08:21:11 +0100

    [ADD] react query

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import SearchProvider from "./providers/SearchProvider.tsx";
 import Routing from "./Routing.tsx";
 import { BrowserRouter } from "react-router";
 import ThemeProvider from "@/providers/ThemeProvider.tsx";
+import QueryProvider from "@/providers/QueryProvider.tsx";
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <ThemeProvider>
-          <SearchProvider>
-            <Routing />
-          </SearchProvider>
-        </ThemeProvider>
-      </BrowserRouter>
+      <QueryProvider>
+        <BrowserRouter>
+          <ThemeProvider>
+            <SearchProvider>
+              <Routing />
+            </SearchProvider>
+          </ThemeProvider>
+        </BrowserRouter>
+      </QueryProvider>
     </>
   );
 }
