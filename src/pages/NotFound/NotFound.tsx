d43fecbac15955c70fdf9386c2c3f38e5c611cff
Maryam.bahangeh <maryam.bahangeh@gmail.com>
2025-04-17 08:26:43 +0200

    [ADD] roadmap page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -4,6 +4,7 @@ import { useTranslation } from "react-i18next";
 function NotFound() {
   const { t } = useTranslation();
 
+
   return <h1 className={styles["not-found"]}>{t("notFound.notFound")}</h1>;
 }
 
