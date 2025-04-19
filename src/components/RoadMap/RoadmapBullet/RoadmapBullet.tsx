d1568db4ccbe283a27ecf03502e37484ab8d2bcd
Maryam.bahangeh <maryam.bahangeh@gmail.com>
2025-04-19 10:51:23 +0200

    [ADD] translation for RoadmapBullet component

--- src/components/RoadMap/RoadmapBullet/RoadmapBullet.tsx
+++ src/components/RoadMap/RoadmapBullet/RoadmapBullet.tsx
@@ -1,5 +1,7 @@
 import styles from "./RoadmapBullet.module.css";
 import clsx from "clsx";
+import { useTranslation } from "react-i18next";
+import { SUGGESTION_STATUS } from "@/dropdown-options/suggestion-status.ts";
 
 export enum BulletSize {
   SMALL = "small",
@@ -13,10 +15,18 @@ type Props = {
 };
 
 function RoadmapBullet({ suggestionStatus, size }: Props) {
+  const { t } = useTranslation();
+
+  const translationKey = SUGGESTION_STATUS.find(
+    (s) => s.value === suggestionStatus,
+  )?.translationKey;
+
+  const translated = t(translationKey as never);
+
   return (
     <div className={clsx(styles["roadmap-bullet"], styles[size])}>
       <span className={clsx(styles.bullet, styles[suggestionStatus])}></span>
-      {suggestionStatus}
+      {translated}
     </div>
   );
 }
